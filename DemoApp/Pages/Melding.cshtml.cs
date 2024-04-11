85b78dc7b7c7d26659e4ad9f28e3f59c355136b5
Bjorge.Saether <bjorge.saether@bufdir.no>
2024-04-11 13:21:46 +0200

    Lagt til "Om" - side, beg. på XSLT

--- DemoApp/Pages/Melding.cshtml.cs
+++ DemoApp/Pages/Melding.cshtml.cs
@@ -1,5 +1,6 @@
 using DemoApp.Models;
 using DemoApp.Models.Fagsystem;
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DemoApp.Pages
@@ -19,10 +20,23 @@ namespace DemoApp.Pages
         public void Init()
         {
             Utils.GetRequestParams(Request, out Dictionary<string, string> queryParams);
+            if (queryParams.ContainsKey("LagHTML"))
+            {
+                // Lagre til XML
+                // Transformere XML v/XSLT
+                // Vise melding
+            }
+            else if (queryParams.ContainsKey("Send"))
+            {
+                // Lagre til XML
+                // Redirect til 
+            }
             SelectedElementType = Utils.GetRequestValue(queryParams, "selectedSchemaName");
             ViewData["Title"] = (SelectedElementType) ?? "Melding";
             PrefillValues = TestdataGenerator.GetPrefillValues(Request);
         }
+
+        public HtmlString TransformedDocument { get; set; }
 
         public List<PrefilledValue>? PrefillValues { get; set; }
         public string MeldingId { get; set; } = "";
